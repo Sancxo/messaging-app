e0e8d78f1a714cfe145de392ff6dad1d01ed1985
Simon Tirant <simon.tirant@gmail.com>
2021-11-26 20:53:03 +0100

    creation de la live post_component.ex

--- lib/messaging_app_web/live/post_live/post_component.ex
+++ lib/messaging_app_web/live/post_live/post_component.ex
@@ -4,6 +4,36 @@ defmodule MessagingAppWeb.PostLive.PostComponent do
   def render(assigns) do
     ~L"""
       <div id="post-<%= @post.id %>">
+        <div>
+          <div>
+            <div></div>
+          </div>
+
+          <div>
+            <b>@<%= @post.username %></b>
+            <br />
+            <%= @post.message %>
+          </div>
+        </div>
+
+        <div>
+          <div>
+            <i class="far fa-heart"></i> <%= @post.likes_count %>
+          </div>
+
+          <div>
+            <i class="far fa-retweet"></i> <%= @post.reposts_count %>
+          </div>
+
+          <div>
+            <%= live_patch to: Routes.post_index_path(@socket, :edit, @post.id) do: %>
+              <i class="far fa-edit"></i>
+            <% end %>
+            <%= link to: "#", phx_click: "delete", phx_value_id: @post.id, data: [confirm: "Are you sure ?"] do: %>
+
+            <% end %>
+          </div>
+        </div>
       </div>
     """
   end
